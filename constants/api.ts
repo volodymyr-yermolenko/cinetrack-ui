3ecad361af91f1565fef6a4b41b23bc399faa8c8
Volodymyr Yermolenko <yermolenko.vol@gmail.com>
2026-03-05 18:52:21 +0200

    Fixed env variables

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,1 @@
-const BASE_API_URL =
-  "https://cinetrack-g3cufqgbd2erh6ad.swedencentral-01.azurewebsites.net/api";
-
-//export const BASE_API_URL = "http://localhost:5000/api";
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
