1e91e66b72846bab1bc443c5324bfaa7d7612f41
Volodymyr Yermolenko <yermolenko.vol@gmail.com>
2026-03-05 18:40:34 +0200

    Corrected API URL

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,4 @@
-//const BASE_API_URL =
-//  "https://cinetrack-g3cufqgbd2erh6ad.swedencentral-01.azurewebsites.net/api";
+const BASE_API_URL =
+  "https://cinetrack-g3cufqgbd2erh6ad.swedencentral-01.azurewebsites.net/api";
 
-export const BASE_API_URL = "http://localhost:5000/api";
+//export const BASE_API_URL = "http://localhost:5000/api";
